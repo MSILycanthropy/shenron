147b26606862977a49640d52f212c501c87af1ad
MSILycanthropy <ethanmichaelk@gmail.com>
2025-12-02 10:56:50 -0600

    basic server struct impl

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,11 @@
-use std::sync::Arc;
+use std::{collections::HashMap, net::SocketAddr, sync::Arc};
 
-use russh::keys::PrivateKey;
+use russh::{
+    Channel, ChannelId,
+    keys::PrivateKey,
+    server::{self, Auth, Config, Msg, Server as _, Session},
+};
+use tokio::sync::Mutex;
 
 use crate::{App, Result};
 
@@ -41,5 +46,157 @@ where
         self.app = Some(Arc::new(app));
 
         self
+    }
+
+    /// # Errors
+    ///
+    /// Will return `Err` if `self.addr` is not set
+    /// Will return `Err` if `self.app` is not set
+    pub async fn serve(self) -> Result<()> {
+        let addr = self
+            .addr
+            .ok_or_else(|| crate::Error::Custom("No bind address specified".into()))?;
+
+        let app = self
+            .app
+            .ok_or_else(|| crate::Error::Custom("No app specified".into()))?;
+
+        if self.keys.is_empty() {
+            return Err(crate::Error::Custom("No host keys specified".into()));
+        }
+
+        let config = Config {
+            keys: self.keys,
+            ..Default::default()
+        };
+
+        let config = Arc::new(config);
+        let mut sh = ShenronServer { app };
+
+        sh.run_on_address(config, addr).await?;
+
+        Ok(())
+    }
+}
+
+struct ShenronServer<A> {
+    app: Arc<A>,
+}
+
+impl<A> server::Server for ShenronServer<A>
+where
+    A: App,
+{
+    type Handler = ShenronHandler<A>;
+
+    fn new_client(&mut self, _addr: Option<SocketAddr>) -> Self::Handler {
+        ShenronHandler {
+            app: Arc::clone(&self.app),
+            channels: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+}
+
+struct ShenronHandler<A> {
+    app: Arc<A>,
+    channels: Arc<Mutex<HashMap<ChannelId, Channel<Msg>>>>,
+}
+
+impl<A> server::Handler for ShenronHandler<A>
+where
+    A: App,
+{
+    type Error = crate::Error;
+
+    async fn channel_open_session(
+        &mut self,
+        channel: Channel<russh::server::Msg>,
+        _session: &mut russh::server::Session,
+    ) -> Result<bool> {
+        tracing::info!("Channel opened: {:?}", channel.id());
+
+        self.channels.lock().await.insert(channel.id(), channel);
+
+        Ok(true)
+    }
+
+    async fn auth_publickey(
+        &mut self,
+        user: &str,
+        _public_key: &russh::keys::ssh_key::PublicKey,
+    ) -> Result<Auth> {
+        tracing::info!("Pubkey auth attempt for user: {}", user);
+
+        // TODO: impl publickey auth
+
+        Ok(Auth::Accept)
+    }
+
+    async fn auth_password(&mut self, user: &str, _password: &str) -> Result<Auth> {
+        tracing::info!("Password auth attempt for user: {}", user);
+
+        // TODO: impl password auth
+
+        Ok(Auth::Accept)
+    }
+
+    async fn pty_request(
+        &mut self,
+        channel: ChannelId,
+        term: &str,
+        col_width: u32,
+        row_height: u32,
+        _pix_width: u32,
+        _pix_height: u32,
+        _modes: &[(russh::Pty, u32)],
+        session: &mut Session,
+    ) -> Result<()> {
+        tracing::info!(
+            "PTY request on channel {:?}: {}x{} ({})",
+            channel,
+            col_width,
+            row_height,
+            term
+        );
+
+        // TODO: impl actually hap handler
+
+        session.channel_success(channel);
+
+        Ok(())
+    }
+
+    async fn data(
+        &mut self,
+        channel: ChannelId,
+        data: &[u8],
+        _session: &mut Session,
+    ) -> Result<()> {
+        tracing::debug!("Data on channel: {:?}: {} bytes", channel, data.len());
+
+        // TODO: forward data to app
+
+        Ok(())
+    }
+
+    async fn window_change_request(
+        &mut self,
+        channel: ChannelId,
+        col_width: u32,
+        row_height: u32,
+        _pix_width: u32,
+        _pix_height: u32,
+        _session: &mut Session,
+    ) -> Result<()> {
+        tracing::debug!(
+            "Window change on channel {:?}: {}x{}",
+            channel,
+            col_width,
+            row_height
+        );
+
+        // TODO: forward window change to app
+
+        Ok(())
     }
 }
